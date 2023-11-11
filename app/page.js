bac05bdb790eab68cd70a1d5ee2f123f59f34d18
echanovevaro <30557018+echanovevaro@users.noreply.github.com>
2023-11-11 18:14:49 +0100

    disable cache on home
    Signed-off-by: echanovevaro <30557018+echanovevaro@users.noreply.github.com>

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import { getAll } from "./actions"
 import MeetupList from "./components/meetups/MeetupList"
-
+export const revalidate = 0
 export default async function HomePage() {
   const data = await getAll()
   return <MeetupList meetups={data} />
