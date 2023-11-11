23c4c4d2f587c7ae3e137b3d9e71bcbab9ced413
echanovevaro <30557018+echanovevaro@users.noreply.github.com>
2023-11-11 17:22:48 +0100

    fix navigation prefetch and styles
    Signed-off-by: echanovevaro <30557018+echanovevaro@users.noreply.github.com>

--- app/components/layout/MainNavigation.js
+++ app/components/layout/MainNavigation.js
@@ -1,22 +1,38 @@
+"use client"
+
 import Link from "next/link"
 import classes from "./MainNavigation.module.css"
+import { usePathname } from "next/navigation"
 
-function MainNavigation() {
+const links = [
+  { name: "All Meetups", href: "/" },
+  {
+    name: "Add New",
+    href: "/new",
+  },
+]
+
+export default function MainNavigation() {
+  const pathname = usePathname()
+  console.log(pathname)
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
-          <li>
-            <Link href="/">All Meetups</Link>
-          </li>
-          <li>
-            <Link href="/new">Add New Meetup</Link>
-          </li>
+          {links.map(({ name, href }) => (
+            <li key={name}>
+              <Link
+                href={href}
+                prefetch={href !== "/"}
+                className={pathname === href ? classes.active : ""}
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
   )
 }
-
-export default MainNavigation
