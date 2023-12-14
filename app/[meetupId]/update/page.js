c22ab94f4f9d255575c2a2358a6cc277e3acc1c5
echanovevaro <echanovevaro@gmail.com>
2023-12-14 13:23:19 +0100

    remove last reference to store

--- app/[meetupId]/update/page.js
+++ app/[meetupId]/update/page.js
@@ -1,11 +1,10 @@
 'use client'
 // import { getOne } from "@/app/services/actions"
 import NewMeetupForm from "@/app/components/meetups/NewMeetupForm"
-import useMeetupsStore from "@/src/store"
 
 export default async function MeetupUpdatePage({ params }) {
   const { meetupId } = params
-  // const data = await getOne(meetupId)
+  const data = await getOne(meetupId)
 
-  return <NewMeetupForm meetup={useMeetupsStore.getState().meetup} />
+  return <NewMeetupForm meetup={data} />
 }
