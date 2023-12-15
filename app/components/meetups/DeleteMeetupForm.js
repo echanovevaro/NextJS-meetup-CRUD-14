b028524e9cdce9c6e578915f82d2c108280cf270
echanovevaro <echanovevaro@gmail.com>
2023-12-15 18:15:10 +0100

    fixes

--- app/components/meetups/DeleteMeetupForm.js
+++ app/components/meetups/DeleteMeetupForm.js
@@ -1,15 +1,17 @@
 import { remove } from "@/app/services/actions"
+import { useState } from "react"
 
 export default function DeleteMeetupForm({ id }) {
-  function handleSubmit(e) {  
-    e.preventDefault()
-    remove(id)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  async function handleSubmit() { 
+    setIsSubmitting(true)
+    await remove(id)
   }
   return (
     <>
       <form action={handleSubmit}>
         <input type="hidden" name="id" defaultValue={id} />
-        <button type="submit">Delete</button>
+        <button type="submit" disabled={isSubmitting}>{isSubmitting ? 'Deleting' : 'Delete'}</button>
       </form>
     </>
   )
