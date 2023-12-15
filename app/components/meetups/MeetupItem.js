d5760dc1f6a3d6a36230fe6929941034fb9e0998
echanovevaro <echanovevaro@gmail.com>
2023-12-15 18:30:44 +0100

    fixes

--- app/components/meetups/MeetupItem.js
+++ app/components/meetups/MeetupItem.js
@@ -26,11 +26,11 @@ function MeetupItem({ meetup }) {
       >
         <h6 className="text-md fw-light">{meetup.title}</h6>
         <h6 className="hidden mb-1">
-                  <div className="d-flex justify-content-end align-items-center gap-2">
+                  <div className="d-flex justify-content-end align-items-start gap-2">
           <p className="text-xs fw-lighter text-right m-0 text-blue"><FontAwesomeIcon icon={faLocationDot}/></p>
         <Card.Text className="text-xs fw-lighter text-right mb-0">{datetime}</Card.Text>
         </div>
-        <div className="d-flex justify-content-end align-items-center gap-2">
+        <div className="d-flex justify-content-end align-items-start gap-2">
           <p className="text-xs fw-lighter text-right m-0 text-blue"><FontAwesomeIcon icon={faClock}/></p>
         <Card.Text className="text-xs fw-lighter text-right mb-0">{meetup.address} {meetup.city}</Card.Text>
         </div>
